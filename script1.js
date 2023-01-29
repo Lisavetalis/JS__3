582e2f1976c05b2572e864a37b9217f06af44872
Lisaveta <lisavetagolovataya@gmail.com>
2023-01-30 00:38:52 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,48 @@
+//1
+/*for (let i = 1; i <= 50; i++) {
+    console.log(i);
+}*/
+
+/*for (let i = 35; i >= 8; i--) {
+    console.log(i);
+};*/
+
+//3
+/*let sum = 0;
+for (let i = 0; i <= 100; i++) {
+    sum += i;
+    if (i == 100) {
+    console.log(sum);
+    }
+}*/
+
+
+//4
+/*
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum += i;
+    console.log(sum);
+}*/
+
+//5
+
+/*for (let i = 8; i <= 56; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}*/
+
+//7
+/*let num = 0;
+let n = 1000;
+for (; n-=0;) {
+    num = num + 1;
+    n = n / 2;
+    if (n < 50) {
+        console.log(n);
+        break;
+    }
+    console.log(num);
+}*/
+
